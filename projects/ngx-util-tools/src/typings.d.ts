17d5f9dfcc44175d167174caa6238dafe85ce152
robmanganelly <robmanganelly@gmail.com>
2022-07-06 20:17:44 -0400

    adding typings.d.ts

--- projects/ngx-util-tools/src/typings.d.ts
+++ projects/ngx-util-tools/src/typings.d.ts
@@ -1,8 +1,12 @@
-declare module 'host' {
-  export interface Host {
-    protocol?: string;
-    hostname?: string;
-    pathname?: string;
-  }
-  export function parse(url: string, queryString?: string): Host;
-}
+// declare module 'host' {
+//   export interface Host {
+//     protocol?: string;
+//     hostname?: string;
+//     pathname?: string;
+//   }
+//   export function parse(url: string, queryString?: string): Host;
+// }
+//! In the component or file that uses the library, add the following code:
+// import * as host from 'host';
+// const parsedUrl = host.parse('https://angular.io');
+// console.log(parsedUrl.hostname);
